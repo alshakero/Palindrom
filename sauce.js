ff956b6a4ba9681248b55448ab150ae42e759f53
Omar Alshaker <omar@omaralshaker.com>
2017-03-31 15:29:42 +0200

    Travis test

---
+++ sauce.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver'),
+    username = process.env.SAUCE_USERNAME,
+    accessKey = process.env.SAUCE_ACCESS_KEY,
+    driver;
+    
+var caps = {
+    'browserName': 'chrome',
+    'platform': 'Windows 10',
+    'version': '49.0',
+    'username': username,
+    'accessKey': accessKey,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+};
+
+driver = new webdriver.Builder().
+  withCapabilities(caps).
+  usingServer("http://" + username + ":" + accessKey +
+              "@ondemand.saucelabs.com:80/wd/hub").
+  build();
+
+driver.getTitle().then(function (title) {
+    console.log("title is: " + title);
+});
+
+driver.quit();
